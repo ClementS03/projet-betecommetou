ff5214ad5017c6bef319322d135cc3ca8dd4ef8e
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2020-04-13 12:44:18 +0200

    reglage d'un bug sur le blur du JS

--- Wordpress/content/themes/Betecommetou/app/js/app.js
+++ Wordpress/content/themes/Betecommetou/app/js/app.js
@@ -9,12 +9,12 @@ var app = {
   },
   handleOpenFrontPageMenu: function () {
     document.querySelector('.open-menu').style.visibility = "hidden";
-    document.querySelector('.main').style.filter = "blur(1.5rem)";
+    document.querySelector('.wrapper').style.filter = "blur(1.5rem)";
     document.querySelector('.header__menu').style.visibility = "visible";
   },
   handleCloseFrontPageMenu: function () {
     document.querySelector('.open-menu').style.visibility = "visible";
-    document.querySelector('.main').style.filter = "";
+    document.querySelector('.wrapper').style.filter = "";
     document.querySelector('.header__menu').style.visibility = "hidden";
   }
 };
