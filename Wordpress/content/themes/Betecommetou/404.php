2bdc85befefe8223301067a93e25be2476bbf28b
Nicolas-Szelendar <szelendarnicolas@gmail.com>
2020-04-15 17:22:55 +0200

    integration page 404 not finished

--- Wordpress/content/themes/Betecommetou/404.php
+++ Wordpress/content/themes/Betecommetou/404.php
@@ -1,9 +1,17 @@
-/**
- * The template for displaying 404 pages (Not Found)
- */
-<? get_header(); ?>
+<?php get_header(); ?>
 
-<h1>Erreur 404</h1>
- <p>Oups, ceci est une erreur dite 404 ! En termes simples, la page ne peut être trouvée... Je vous invite dès à présent à utiliser les outils de recherche et / ou de navigation pour parvenir au contenu souhaité.</p>
+
+<div>
+
+
+<h1 style="padding:3em;color:#D57100; font-weight:bold; font-size: 3em;">Erreur 404</h1>
+ <p style="padding:1em;color:#D57100; font-weight:bold; font-size: 2em;">Faites demi-tour *VoixduGPS*  </p>
+ </div>
+
+
+
+
+
+
 
 <?php get_footer(); ?>
