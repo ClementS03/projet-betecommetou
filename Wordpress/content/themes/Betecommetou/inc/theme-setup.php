677fadbd6f927e13353ab35a101e252e8aeb46d8
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2020-04-16 16:10:19 +0200

    some arrangements

--- Wordpress/content/themes/Betecommetou/inc/theme-setup.php
+++ Wordpress/content/themes/Betecommetou/inc/theme-setup.php
@@ -14,24 +14,6 @@ if (!function_exists('betecommetou_setup')) {
     add_action('after_setup_theme', 'betecommetou_setup');
 
 
-// Redirection if not logged    
-if (!function_exists('redirect_if_not_logged')) {
-
-    function redirect_if_not_logged () {
-
-            if (!is_user_logged_in() && is_front_page() || is_single() || is_archive() || is_page('login')) {
-
-            } elseif (is_user_logged_in()) {
-
-            }  else {
-                wp_safe_redirect(home_url() . '/login');
-                    exit(); 
-            }
-        
-    }
-}
-add_action('template_redirect', 'redirect_if_not_logged');
-
 // For don't show admin bar
 function remove_admin_bar() {
     if (!current_user_can('edit_posts') && !is_admin()) {
@@ -40,12 +22,3 @@ function remove_admin_bar() {
 }
 add_action('after_setup_theme', 'remove_admin_bar');
 
-//Redirection on home page if user logged
-function redirect_from_login_if_user_is_logged() {
-    if(is_page('login') && is_user_logged_in()) {
-        if (!is_front_page()) {
-            wp_safe_redirect(home_url());
-        }
-    }
-}
-add_action('template_redirect', 'redirect_from_login_if_user_is_logged');
