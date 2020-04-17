4e2dd0d58a3d9109cfab65aa15025ea9d0da8b08
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2020-04-17 11:00:55 +0200

    add options more dynamic to find the good pages

--- Wordpress/content/themes/Betecommetou/inc/redirection.php
+++ Wordpress/content/themes/Betecommetou/inc/redirection.php
@@ -1,31 +1,31 @@
 <?php
 // Redirection if not logged    
-// if (!function_exists('redirect_if_not_logged')) {
+if (!function_exists('redirect_if_not_logged')) {
 
-//     function redirect_if_not_logged () {
+    function redirect_if_not_logged () {
 
-//             if (!is_user_logged_in() && is_front_page() || is_single() || 
-//             is_archive() || is_page([56,40])) {
+            if (!is_user_logged_in() && is_front_page() || is_single() || 
+            is_archive() || is_page(['inscription','login'])) {
 
-//             } elseif (is_user_logged_in()) {
+            } elseif (is_user_logged_in()) {
 
-//             }  else {
-//                 wp_safe_redirect(home_url() . '/login');
-//                     exit(); 
-//             }
+            }  else {
+                wp_safe_redirect(home_url() . '/login');
+                    exit(); 
+            }
         
-//     }
-// }
-// add_action('template_redirect', 'redirect_if_not_logged');
+    }
+}
+add_action('template_redirect', 'redirect_if_not_logged');
 
-//Redirection on home page if user logged
-// function redirect_from_login_if_user_is_logged() {
-//     if(is_page([56,40]) && is_user_logged_in()) {
-//         if (!is_front_page()) {
-//             wp_safe_redirect(home_url());
-//         }
-//     }
-// }
-// add_action('template_redirect', 'redirect_from_login_if_user_is_logged');
+// Redirection on home page if user logged
+function redirect_from_login_if_user_is_logged() {
+    if(is_page(['inscription','login']) && is_user_logged_in()) {
+        if (!is_front_page()) {
+            wp_safe_redirect(home_url());
+        }
+    }
+}
+add_action('template_redirect', 'redirect_from_login_if_user_is_logged');
 
 ?>
