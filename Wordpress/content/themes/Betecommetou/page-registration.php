d26c77a986dfd2a0170bfd06f7f26f1271227d99
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2020-04-21 15:03:46 +0200

    conflict resolution

---
+++ Wordpress/content/themes/Betecommetou/page-registration.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+Template Name: registration
+*/
+?>
+<?php get_header(); ?>
+
+<h2><?php the_title() ?></h2>
+
+<?= do_shortcode( '[user_registration_form id="53"]' ); ?>
+<?php get_footer(); ?>
