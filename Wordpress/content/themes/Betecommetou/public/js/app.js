84879de73864a3fe2a14e0e0b68188f03f045dbe
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2020-04-23 10:15:56 +0200

    test server

--- Wordpress/content/themes/Betecommetou/public/js/app.js
+++ Wordpress/content/themes/Betecommetou/public/js/app.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9dcef25caaf1d6464875";
+/******/ 	var hotCurrentHash = "bfcc022c5bb6f5700965";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -867,8 +867,8 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function(axios) {var app = {
-
-  baseUri: "http://ec2-52-90-30-182.compute-1.amazonaws.com/projet-betecommetou/Wordpress",
+  
+  baseUri: "http://ec2-52-90-30-182.compute-1.amazonaws.com/projet-betecommetou/Wordpress/",
   jsonUrl:"wp-json/wp/v2/",
   jwtUrl: "wp-json/jwt-auth/v1/",
 
