8e7897c8c169bfcf0e59d009701c1d0143a407e9
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2020-04-23 16:53:32 +0200

    adding links to contact in footer

--- Wordpress/content/themes/Betecommetou/footer.php
+++ Wordpress/content/themes/Betecommetou/footer.php
@@ -3,7 +3,7 @@
     <div class="footer__newsletter">
         <div class="links">
         <ul class="links__list">
-            <li class="links__contact"><a href="#">Contact</a></li>
+            <li class="links__contact"><a href="<?= get_permalink(get_page_by_path('contact')); ?>">Contact</a></li>
             <li class="links__link"><a href="#">Newsletter</a></li>
             <li class="links__legal_mentions"><a href="<?= get_permalink(get_page_by_path('mentions-legales')); ?>">Mentions légales</a></li>
             <li class="links__equipe"><a href="<?= get_permalink(get_page_by_path('lequipe')); ?>">L'equipe</a></li>
