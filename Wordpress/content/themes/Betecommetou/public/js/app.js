369aa9e15f36ad318fe2accaee43d214be6e43aa
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2020-04-23 17:09:24 +0200

    adding links to contact in footer

--- Wordpress/content/themes/Betecommetou/public/js/app.js
+++ Wordpress/content/themes/Betecommetou/public/js/app.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b0c4b83fa124855b110e";
+/******/ 	var hotCurrentHash = "265467d7fc65fe214579";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
