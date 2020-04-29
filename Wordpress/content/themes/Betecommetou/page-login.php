5ea998622063a0febcbe20288958bdfacc259261
Floriane-Pesseas <florianepesseas1@gmail.com>
2020-04-29 12:25:34 +0200

    next

--- Wordpress/content/themes/Betecommetou/page-login.php
+++ Wordpress/content/themes/Betecommetou/page-login.php
@@ -1,35 +1,35 @@
- <!-- <?php 
+ <?php 
 
 /*
 Template Name: login
 */
-/*get_header();*/
-// wp_login_form([
-//     'redirect' => get_page_link(get_page_by_title('votre compte')->ID),
-//     'remember' => false,
-// ]);
+get_header();
+wp_login_form([
+    'redirect' => get_page_link(get_page_by_title('votre compte')->ID),
+    'remember' => false,
+]);
 ?>
     
     <?php
-		// $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-		// if (strpos($url, 'login/?champs=vides')!==false) {
-		// 	echo "<div class='login_failed'>Champs d'identifiant vide</div>";
-		// }
-		// if (strpos($url, 'login/?password=vide')!==false) {
-		// 	echo "<div class='login_failed'>Champs de mot de passe vide</div>";
-		// }
-		// if (strpos($url, 'login/?utilisateur=vide')!==false) {
-		// 	echo "<div class='login_failed'>Champs d'identifiant et de mot de passe vide</div>";
-		// }	
+		$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+		if (strpos($url, 'login/?champs=vides')!==false) {
+			echo "<div class='login_failed'>Champs d'identifiant vide</div>";
+		}
+		if (strpos($url, 'login/?password=vide')!==false) {
+			echo "<div class='login_failed'>Champs de mot de passe vide</div>";
+		}
+		if (strpos($url, 'login/?utilisateur=vide')!==false) {
+			echo "<div class='login_failed'>Champs d'identifiant et de mot de passe vide</div>";
+		}	
 	?>
     <?php
-		// if (strpos($url,'login/?login=echec') !== false) {
-		// 	echo "<div class='login_failed'>Mot de passe ou nom d'utilisateur incorrect</div>";
-		// }
+		if (strpos($url,'login/?login=echec') !== false) {
+			echo "<div class='login_failed'>Mot de passe ou nom d'utilisateur incorrect</div>";
+		}
 	?>
 	
 	
 
 <?php
-// get_footer();
-?> -->
+get_footer();
+?> 
