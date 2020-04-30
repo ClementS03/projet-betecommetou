bf88613dc3e4430a082423c5f70080f78f78d89c
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2020-04-30 10:20:45 +0200

    maj serveur

--- Wordpress/content/themes/Betecommetou/app/js/app.js
+++ Wordpress/content/themes/Betecommetou/app/js/app.js
@@ -1,7 +1,7 @@
 var app = {
 
   // URL and endpoints for request to API
-  baseUri: "http://ec2-52-90-30-182.compute-1.amazonaws.com/projet-betecommetou/Wordpress/",
+  baseUri: "http://3.213.90.111/projet-betecommetou/Wordpress/",
   //baseUri: "http://localhost/Projet/projet-betecommetou/Wordpress/",
   jsonUrl:"wp-json/wp/v2/",
   jwtUrl: "wp-json/jwt-auth/v1/",
